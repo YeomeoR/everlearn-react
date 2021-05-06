c1e1eb13ac772a6bb4dacb920f4a64b517d6a6d4
Rob Yeomans <76974577+YeomeoR@users.noreply.github.com>
2021-05-06 16:41:50 +0100

    Bootstrap and Firebase working
    Bootstrap basic style (dark) rendering out the collection from firebase. Talking to each other.
    Covid jab giving me big symptoms!

--- App.js
+++ App.js
@@ -1,26 +1,73 @@
-import { useState, useEffect } from 'react';
 import './App.css';
+import { Table } from 'react-bootstrap';
 import firebase from './firebase';
+import React from 'react';
 
 function App() {
-  const [learners, setLearners] = useState([]);
+
+  const [learners, setLearners] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const db = firebase.firestore();
+      const data = await db.collection('learners').get();
+      setLearners(data.docs.map(doc => doc.data()))
+    }
+    fetchData();
+  }, []); // end useEffect
 
 
-    useEffect(() => {
-      const fetchData = async () => {
-        const db = firebase.firestore();
-        const data = await db.collection('learners').get();
-        setLearners(data.docs.map(doc => doc.data()))
-      }
-      fetchData()
-    }, []);
+  // const learners = [
+  //   {
+  //     firstName: 'Robert',
+  //     lastName: 'Yeomans',
+  //     email: 'email@yeomeo.dev',
+  //     score: '10',
+  //   },
+  //   {
+  //     firstName: 'Olivia',
+  //     lastName: 'Neame',
+  //     email: 'olivia@yeomeo.dev',
+  //     score: '9',
+  //   },
+  //   {
+  //     firstName: 'Alex',
+  //     lastName: 'Pullen',
+  //     email: 'alex@yeomeo.dev',
+  //     score: '7',
+  //   },
+  //   {
+  //     firstName: 'Harry',
+  //     lastName: 'Smith',
+  //     email: 'harry@yeomeo.dev',
+  //     score: '7',
+  //   },
+  // ];
 
   return (
-    <ul>
-      {learners.map(learner => (
-        <li key={learner.id}>{learner.firstName} {learner.lastName} {learner.email} {learner.score}</li>
-      ))}
-    </ul>
+    <div className="App">
+      <h1>Everlearn Holding</h1>
+      <Table striped bordered hover variant="dark">
+        <tbody>
+          <tr>
+            <td>ID</td>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Email</td>
+            <td>Score</td>
+          </tr>
+          {learners.map((item, i) => (
+            <tr key={i}>
+              <td>{i + 1}</td>
+              <td>{item.firstName}</td>
+              <td>{item.lastName}</td>
+              <td>{item.email}</td>
+              <td>{item.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 }
 
